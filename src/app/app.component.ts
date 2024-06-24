064cdba9ab7a136f3f7b0fd977777f6759d9dd34
Philippe <borgodoff@gmail.com>
2024-06-24 20:57:40 +0200

    Default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<h1>Welcome!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'home';
 }
